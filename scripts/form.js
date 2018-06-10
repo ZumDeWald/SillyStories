5b7c8404898af73093da57d251f08ba66d01fd04
ZumDeWald <zumdewald@gmail.com>
2018-06-10 11:26:56 -0500

    Form submission captured
    JS will capture form input
    Input will be placed in story
    Has to be manually changed at this point
    Will not dynamically generate stories

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,39 @@
 $(document).ready(() => {
 
-    /* VARIABLES */
+  /* VARIABLES */
+let $propName = "";
+let $adj1 = "";
+let $adj2 ="";
+let $verb = "";
+let $adv = "";
+
+
+const $submitButton = $("#submit");
+const $story = $(".story");
 
 
 
 
-    /* FUNCTIONS */
+  /* FUNCTIONS */
+
+  $submitButton.click((e) => {
+    $("form").submit();
+    $propName = $("#propName").val();
+    $adj1 = $("#adj1").val();
+    $adj2 = $("#adj2").val();
+    $verb = $("#verb").val();
+    $adv = $("#adv").val();
+//    console.log($propName);
+    (e).preventDefault;
+
+    /* Story Generator */
+
+    $story.html("<p> Hello, my name is " + $propName + ". I have a " + $adj1 + " car and a " + $adj2 + " house. I " + $verb + " very " + $adv + "!");
+
+  })
+
+
+
 
 
 
