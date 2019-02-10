6d18f3482a2986b52d56c136a015d0f4701bfcc1
ZumDeWald <zumdewald@gmail.com>
2019-02-10 15:32:06 -0600

    Sliding menu opens and closes

--- silly-stories/src/components/Menu.js
+++ silly-stories/src/components/Menu.js
@@ -5,12 +5,28 @@ const Menu = (props) => {
   const [menuOpen, setMenuChange] = useState(false);
 
   const changeMenuState = () => {
-    (!!menuOpen) ? setMenuChange(false) : setMenuChange(true);
+    (!!menuOpen) ?
+    toggleMenu("close") :
+    toggleMenu("open");
   };
 
+  const toggleMenu = (menuState) => {
+    const grabMenu = document.getElementById("menu");
+    const grabIcon = document.getElementsByClassName("menu-icon");
+    if (menuState === "close") {
+      grabMenu.classList.add("closed");
+      grabIcon[0].classList.add("rotate-90");
+      setMenuChange(false);
+    } else {
+      grabMenu.classList.remove("closed");
+      grabIcon[0].classList.remove("rotate-90");
+      setMenuChange(true);
+    }
+  }
+
   return (
-    <section id="menu">
-    <i className="fas fa-chevron-circle-down menu-open"
+    <section id="menu" className="closed">
+    <i className="fas fa-chevron-circle-down menu-icon rotate-90"
        onClick={() => {changeMenuState()}}></i>
       <ul className="menu-list">
         <li className="menu-item">
