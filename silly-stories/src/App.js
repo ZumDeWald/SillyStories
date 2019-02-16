40cef1eb15b6e80f19101a6367a6adc5a193ca9f
ZumDeWald <zumdewald@gmail.com>
2019-02-16 15:45:43 -0600

    REFACTOR Remove dead code and files

--- silly-stories/src/App.js
+++ silly-stories/src/App.js
@@ -20,9 +20,6 @@ class App extends Component {
       story: ["Choose Story", null]
     }
 
-    // //Bindings
-    // this.setFormSubmissions = this.setFormSubmissions.bind(this);
-    // this.formSubmit = this.formSubmit.bind(this);
   }
 
   setStory = (newStory) => {
@@ -34,28 +31,6 @@ class App extends Component {
     this.setState({ showStory: false });
   }
 
-  /* setState to add each entry from the form submission into it's own array with the name and value of the entry. This will be passed to the FinalStory component and used to replace the placeholder words. */
-  // setFormSubmissions = (targetName, targetValue) => {
-  //   this.setState(prevState => ({
-  //     formValues: [...prevState.formValues,
-  //                 [targetName, targetValue] ]
-  //   }));
-  // }
-
-  /* Retrieves form data from Form component and adds each entry to local state */
-  // formSubmit = (e) => {
-  //   e.preventDefault();
-  //   const target = e.target;
-  //   /* Loop over form data and extract each input value into a new entry in local state.
-  //   Unable to get for...in loop to work, had to use regular for loop and then have the index to access each entry in the form data. */
-  //   for (let i = 0; i < target.length; i++) {
-  //     let targetName = target[i].name;
-  //     targetName = targetName.replace(/[{}]/g, "");
-  //     let targetValue = target[i].value;
-  //     this.setFormSubmissions(targetName, targetValue);
-  //   };
-  //   console.log(this.state.formValues);
-  // }
 
   render() {
     return (
--- silly-stories/src/components/Selection.js
+++ silly-stories/src/components/Selection.js
@@ -3,13 +3,16 @@ import Stories from './Stories.js';
 
 const Selection = (props) => {
 
+  //Destructuring
+  const { handleSetStory } = props;
+
   return (
     <section id="selection" className="flex-col-center">
       <h3 className="header-text bottom-line selection-header">Choose your adventure</h3>
       <ul className="no-list-style selection-list">
         {Stories.map((story, index) => (
           <li className="selection-list-item flex-col-center"
-              onClick={() => {props.handleSetStory(story)}}
+              onClick={() => {handleSetStory(story)}}
               key={index}>
             {story[0]}
           </li>
