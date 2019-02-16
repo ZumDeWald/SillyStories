c4806d326f84fe20e078f0d1641d7acf24e86671
ZumDeWald <zumdewald@gmail.com>
2019-02-16 15:05:13 -0600

    Style Story header

--- silly-stories/src/components/Story.js
+++ silly-stories/src/components/Story.js
@@ -7,8 +7,9 @@ const Story = (props) => {
 
   return (
     <section id="story">
-      <p>Fill in the spaces and then hit the button to reveal your new Silly Story!</p>
-      <h3>{chosenStory[0]}</h3>
+      <p className="directions">Fill in spaces</p>
+      <p className="directions">Reveal new story</p>
+      <h3 className="chosenStory">/ / {chosenStory[0]}</h3>
       {chosenStory[1]}
     </section>
   );
