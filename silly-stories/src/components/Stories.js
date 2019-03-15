d8d2d791ae50861ba2cde07ff54b8a47a22c6c21
ZumDeWald <zumdewald@gmail.com>
2019-03-15 06:38:32 -0500

    Minor change in AboutMe

--- silly-stories/src/components/Stories.js
+++ silly-stories/src/components/Stories.js
@@ -1,7 +1,7 @@
 
 const Stories = [
   ["About Me",
-    "Hello, my name is _name ! I have a _noun , _number _noun s, and a _noun . My biggest focus lately has been my _noun . _verb over _number hours a day has kept me _adjective . Hopefully my _adjective skills in _skill will get me a new _noun . My _sibling thinks I am _feeling and should be _verb instead."
+    "Hello, my name is _name ! I have a _noun , _number _noun s, and a _noun . My biggest focus lately has been my _noun . _verb ing over _number hours a day has kept me _adjective . Hopefully my _adjective skills in _skill will get me a new _noun . My _sibling thinks I am _feeling and should be _verb instead."
   ],
 
   ["Favorite Game",
