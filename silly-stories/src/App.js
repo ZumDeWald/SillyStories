7b277bfc1c2cb0cd5d63cd38f362c6eeaf3a0b50
ZumDeWald <zumdewald@gmail.com>
2019-05-07 06:33:57 -0500

    Added two stripes to header

--- silly-stories/src/App.js
+++ silly-stories/src/App.js
@@ -1,55 +1,57 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 //Components
-import Menu from "./components/Menu.js";
-import Selection from "./components/Selection.js";
-import Story from "./components/Story.js";
-import Glossary from "./components/Glossary.js";
+import Menu from './components/Menu.js';
+import Selection from './components/Selection.js';
+import Story from './components/Story.js';
+import Glossary from './components/Glossary.js';
 
 //Stylesheets
-import "./stylesheets/App.css";
-import "./stylesheets/Menu.css";
-import "./stylesheets/Selection.css";
-import "./stylesheets/Story.css";
-import "./stylesheets/Glossary.css";
+import './stylesheets/App.css';
+import './stylesheets/Menu.css';
+import './stylesheets/Selection.css';
+import './stylesheets/Story.css';
+import './stylesheets/Glossary.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: "Selection",
-      story: ["Choose Story", null]
+      display: 'Selection',
+      story: ['Choose Story', null],
     };
   }
 
   //Set state to selected story and render chosen component.
   setStory = (newStory, component) => {
-    this.setState({ story: newStory });
-    this.setState({ display: component });
+    this.setState({story: newStory});
+    this.setState({display: component});
   };
 
   //Set state to chosen component, this updates which component is rendered.
   changeDisplay = component => {
-    this.setState({ display: component });
+    this.setState({display: component});
   };
 
   render() {
     return (
-      <main id="top-level-container">
-        <header className="App-header">
+      <main id='top-level-container'>
+        <header className='App-header'>
           <p>Silly Stories</p>
+          <hr />
+          <hr />
         </header>
         <Menu
           handleChangeDisplay={this.changeDisplay}
           handleSetStory={this.setStory}
         />
         {/* Ternary with multiple options. Checks state for which component to render. */}
-        {this.state.display === "Selection" ? (
+        {this.state.display === 'Selection' ? (
           <Selection handleSetStory={this.setStory} />
-        ) : this.state.display === "Story" ? (
+        ) : this.state.display === 'Story' ? (
           <Story chosenStory={this.state.story} />
-        ) : this.state.display === "Glossary" ? (
+        ) : this.state.display === 'Glossary' ? (
           <Glossary />
         ) : (
           <p>Please choose a story or Glossary</p>
@@ -62,7 +64,7 @@ class App extends Component {
 
 /* PROPTYPES */
 App.propTypes = {
-  setStory: PropTypes.func
+  setStory: PropTypes.func,
 };
 
 export default App;
